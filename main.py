da961a3554392c7a667f2b2c3d0139fd6d2174fc
yassi-github <s19t312@stu.kagawa-u.ac.jp>
2019-12-08 20:04:35 +0900

    age

--- main.py
+++ main.py
@@ -1,29 +1,54 @@
+
 from kivy.app import App
 from kivy.uix.widget import Widget
-# エラー出るんだが使えてる…？
-from kivy.properties import StringProperty
+from kivy.uix.boxlayout import BoxLayout
+from kivy.properties import StringProperty, ListProperty, ObjectProperty
+from kivy.uix.label import Label
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.textinput import TextInput
+from kivy.uix.screenmanager import ScreenManager, Screen, FadeTransition
+from kivy.lang import Builder
+from kivy.config import Config
+Config.set('graphics', 'width', '640')
+Config.set('graphics', 'height', '480')
 
-class TextWidget(Widget):
-    tex = StringProperty()
+
+class RootWidget(ScreenManager):
+    pass
+        
+class Testwidget(Screen):
+    text  = StringProperty()
+    label1 = ObjectProperty()
 
     def __init__(self, **kwargs):
-        super(TextWidget, self).__init__(**kwargs)
-        self.tex = "yo"
+        super(Testwidget, self).__init__(**kwargs)
+        self.text = 'start'
 
-    def buttonclicked(self):
-        self.tex = "Hello"
+    def buttonClicked(self):
+        self.text = 'hello'
 
-    def buttonclicked2(self):
-        self.tex = "world"
+    def buttonClicked2(self):
+        self.text = 'hello2'
+
+    def buttonClicked3(self):
+        self.text = 'hello3'
+
+class SecSc(Screen):
+    l0 = ObjectProperty(None)
+    t0 = ObjectProperty(None)
+
+    def txtmodify(self):
+        self.ids.l0.text = self.ids.t0.text
+
+kv = Builder.load_file("test.kv")
 
 class TestApp(App):
     def __init__(self, **kwargs):
         super(TestApp, self).__init__(**kwargs)
-        self.title = "testing"
+        self.title = "Testing..."
 
     def build(self):
-        return TextWidget()
+        return kv
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     TestApp().run()
